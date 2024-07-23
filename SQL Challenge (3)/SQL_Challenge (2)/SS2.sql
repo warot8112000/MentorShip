408bd3aa7928c9a5a1b9a1a765b1bd303311c64c
Thang Tran <125805997+warot8112000@users.noreply.github.com>
2024-07-23 19:47:18 +0700

    Programing
    update for Programing

--- SQL Challenge (3)/SQL_Challenge (2)/SS2.sql
+++ SQL Challenge (3)/SQL_Challenge (2)/SS2.sql
@@ -44,14 +44,38 @@ CREATE TABLE runner (
 --2. Organize Runners Into Groups
 --Select the main distance and the number of runners that run the given distance (runners_number). 
 --Display only those rows where the number of runners is greater than 3.
-SELECT
+SELECT COUNT(*)
+FROM runner
+WHERE main_distance > 3
+GROUP BY runner_id, main_distance
+
+SELECT *
+FROM runner_event
+
+SELECT *
+FROM event
 
 /*
 3.How Many Runners Participate in Each Event
 Display the event name and the number of club members that take part in this event (call this column runner_count). 
 Note that there may be events in which no club 
-members participate. For these events, the runner_count should equal 0.
-
+members participate. For these events, the runner_count should equal 0. */
+SELECT E.name, COUNT(*) AS RUNNER_COUNT
+FROM event AS E
+LEFT JOIN runner_event AS RE ON E.event_id = RE.event_id
+GROUP BY RE.event_id, E.name
+/*
 4.Group Runners by Main Distance and Age
 Display the distance and the number of runners there are for the following age categories: under 20, 20–29, 30–39, 40–49, and over 50. 
 Use the following column aliases: under_20, age_20_29, age_30_39, age_40_49, and over_50.*/
+SELECT *
+FROM runner
+
+SELECT main_distance, 
+SUM(CASE WHEN age < 20 THEN 1 ELSE 0 END) AS under_20,
+SUM(CASE WHEN age >= 20 AND age <= 29 THEN 1 ELSE 0 END) AS F20_29,
+SUM(CASE WHEN age >= 30 AND age <= 39 THEN 1 ELSE 0 END) AS F30_39,
+SUM(CASE WHEN age >= 40 AND age <= 49 THEN 1 ELSE 0 END) AS F40_49,
+SUM(CASE WHEN age > 50 THEN 1 ELSE 0 END) AS OVER50
+FROM runner
+GROUP BY main_distance	

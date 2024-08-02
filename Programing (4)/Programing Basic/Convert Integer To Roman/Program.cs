af492fafed0f871ea650b346df8e6ac96bc027f3
Thang Tran <125805997+warot8112000@users.noreply.github.com>
2024-08-02 14:47:44 +0700

    Push programming exercise
    Push programming exercise

---
+++ Programing (4)/Programing Basic/Convert Integer To Roman/Program.cs
@@ -0,0 +1,44 @@
+﻿
+string romanNumber;
+int number;
+do
+{
+    Console.WriteLine("Enter a roman number: ");
+    romanNumber = Console.ReadLine();
+} while (!IsIntegerValid(romanNumber, out number));
+
+romanNumber = ConvertIntegerToRoman(number);
+Console.WriteLine($"The roman number is: {romanNumber}");
+
+string ConvertIntegerToRoman( int number)
+{
+
+    string[] romanNumbers = new string[] { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };
+    int[] numbers = new int[] { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
+    string result = "";
+
+    for (int i = romanNumbers.Count() - 1; i >= 0; i--)
+    {
+        while (number >= numbers[i])
+        {
+            result += romanNumbers[i];
+            number -= numbers[i];
+        }
+    }
+
+    return result;
+}
+
+bool IsIntegerValid(string? romanNumber, out int number)
+{
+    
+    if (int.TryParse(romanNumber, out number))
+    {
+        if (number < 1 || number > 3999)
+    {
+        return false;
+    }
+        return true;
+    }
+    return false;
+}
---
+++ Programing (4)/Programing Basic/Convert Roman To Integer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {   string romanNumber;
+        int number;
+        do
+        {
+            Console.Write("Enter a Roman number: ");
+            romanNumber = Console.ReadLine()?.Trim();
+        } while (!IsRomanNumberValid(romanNumber));
+
+        number = RomanToInt(romanNumber);
+        Console.WriteLine($"The Roman number {romanNumber} is equal to {number} in decimal.");
+
+    }
+
+    private static bool IsRomanNumberValid(string romanNumber)
+    {
+        if (string.IsNullOrEmpty(romanNumber))
+        {
+            return false;
+        }
+        string pattern = "^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$";
+        return Regex.IsMatch(romanNumber, pattern, RegexOptions.IgnoreCase);
+    }
+
+    static int RomanToInt(string s)
+    {
+        var map = new Dictionary<char, int>
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        int num = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (i + 1 < s.Length && map[s[i]] < map[s[i + 1]])
+            {
+                num -= map[s[i]];
+            }
+            else
+            {
+                num += map[s[i]];
+            }
+        }
+        return num;
+    }
+}

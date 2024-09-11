ff943ed87adb86fe07a43d8c333f7a94249f7255
Thang Tran <125805997+warot8112000@users.noreply.github.com>
2024-09-11 17:03:01 +0700

    Leet code practice

---
+++ LeetCodePractice/LC1-Two Sum/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[][] inputs = {
+            new int[] {1, 2, 3, 4},
+            new int[] {1, 2},
+            new int[] {2, 2},
+            new int[] {-4, -1, -9, 1, -7},
+            new int[] {25, 50, 75, 100, 400}
+        };
+
+        int[] k = {5, 3, 4, -3, 425};
+
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}.\tArray: [{string.Join(", ", inputs[i])}]");
+            Console.WriteLine($"\tk: {k[i]}");
+
+            int[] result = FindSumBinarySearch(inputs[i], k[i]);
+            Console.WriteLine($"\n\tResult: [{string.Join(", ", result)}]");
+            Console.WriteLine('-' + new string('-', 100) + '\n');
+        }
+    }
+
+
+// O(n^2) time | O(1) space
+    public static int[] FindSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[] { i, j };
+                }
+            }
+        }
+
+        return new int[0]; 
+    }
+
+    public static int[] FindSumTwoPointers(int[] nums, int target)
+    {
+        Array.Sort(nums);
+        Console.Write("Element are sorted: ");
+        for(int i = 0; i < nums.Length; i++)
+        {            
+            Console.Write(nums[i] + " ");
+        }
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left < right)
+        {
+           int sum = nums[left] + nums[right];
+           if(sum == target)
+           {
+                return new int[] { left, right };
+              }
+              else if (sum < target)
+              {
+                left++;
+              }
+              else
+              {
+                right--;
+           }
+        }
+
+        return new int[0];
+    }
+
+    public static int BinarySearch(int[] nums, int target)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int[] FindSumBinarySearch(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            int index = BinarySearch(nums, complement);
+            if (index != -1 && index != i)
+            {
+                return new int[] { i, index };
+            }
+        }
+
+        return new int[0];
+    }
+}
---
+++ LeetCodePractice/LC88 - Merge Sorted Array/Program.cs
@@ -0,0 +1,66 @@
+﻿
+class Program {
+    public static void Main(string[] args) {        
+        List<List<int>> nums1 = new List<List<int>>
+        {
+            new List<int> { 23, 33, 35, 41, 44, 47, 56, 91, 105 },
+            new List<int> { 1, 2 },
+            new List<int> { 1, 1, 1 },
+            new List<int> { 6 },
+            new List<int> { 12, 34, 45, 56, 67, 78, 89, 99 }
+        };
+
+        List<List<int>> nums2 = new List<List<int>>
+        {
+            new List<int> { 32, 49, 50, 51, 61, 99 },
+            new List<int> { 7 },
+            new List<int> { 1, 2, 3, 4 },
+            new List<int> { -99, -45 },
+            new List<int> { 100 }
+        };
+
+        for (int i = 0; i < nums1.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}.\tFirst array: {string.Join(", ", nums1[i])}");
+            Console.WriteLine($"\tSecond array: {string.Join(", ", nums2[i])}");
+
+            int m = nums1[i].Count;
+            int n = nums2[i].Count;
+            var mergedArray = ThreePointers(nums1[i], m, nums2[i], n);
+            Console.WriteLine($"\tMerged array: {string.Join(", ", mergedArray)}");
+            Console.WriteLine(new string('-', 100));
+        }
+    }
+
+public static List<int> ThreePointers(List<int> nums1, int m, List<int> nums2, int n)
+  {
+    if (n == 0) return nums1;
+    for (int a = 0; a < n; a++)
+        {
+            nums1.Add(0);
+        }
+    int i = m - 1;
+    int j = n - 1;
+    int k = m + n - 1;
+    while (i >= 0 && j >= 0)
+    {
+      if (nums1[i] > nums2[j])
+      {
+        nums1[k--] = nums1[i--];
+      }
+      else
+      {
+        nums1[k--] = nums2[j--];
+      }
+    }
+
+    while (j >= 0)
+    {
+      nums1[k--] = nums2[j--];
+    }
+    return nums1;
+  }  
+
+}
+    
+

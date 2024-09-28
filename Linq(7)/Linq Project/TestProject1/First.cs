dd929fd864e7a18549396adacca606808238c928
Thang Tran <125805997+warot8112000@users.noreply.github.com>
2024-09-28 21:36:49 +0700

    UnitTest for first and select method

---
+++ Linq(7)/Linq Project/TestProject1/First.cs
@@ -0,0 +1,27 @@
+﻿using LinqTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class First
+    {
+        [Fact]
+        public void First_ReturnsFirstMatchingElement()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4 };
+            var result = LinQ.From(numbers).First(x => x % 2 == 0);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void First_ThrowsInvalidOperationException()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            Assert.Throws<InvalidOperationException>(() => LinQ.From(numbers).First(x => x > 10));
+        }
+    }
+}
---
+++ Linq(7)/Linq Project/TestProject1/Select.cs
@@ -0,0 +1,58 @@
+﻿using LinqTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class Select
+    {
+        public static List<Student> GetSampleStudents()
+        {
+            return new List<Student>
+        {
+                new Student("John Doe", 22, "Male", "123 Main St", 3.75),
+                new Student("Jane Smith", 20, "Female", "456 Elm St", 3.85),
+                new Student("Tom Brown", 21, "Male", "789 Oak St", 3.95),
+                new Student("Emily Davis", 21, "Female", "321 Pine St", 2.85),
+                new Student("Michael Johnson", 23, "Male", "654 Maple St", 3.65)
+        };
+        }
+
+
+        [Fact]
+        public void Select_WorkCorrectly_WithValidData()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, };
+
+            List<int> result = LinQ.From(numbers).Select(x => x * 2).ToList();
+
+            List<int> expected = new List<int> { 2, 4, 6 };
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Select_ReturnEmptyList_WhenSourceIsEmpty()
+        {
+            List<int> numbers = new List<int> {};
+
+            List<int> result = LinQ.From(numbers).Select(x => x * 2).ToList();
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Select_NullElementsInSource()
+        {
+            List<string> strings = new List<string> { "Hello", null, "World" };
+            List<int> result= LinQ.From(strings).Select(s => s?.Length ?? 0).ToList();
+
+            List<int> expected = new List<int> { 5, 0, 5 };
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
